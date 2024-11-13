d1dbf5f0ff825f2090373770172a1b35d27ffb32
Souhail9397 <souhail.aitouarraou@gmail.com>
2024-11-13 10:52:43 +0100

    Update addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,7 +1,27 @@
-# Exercice 2 - Script de création d'utilisateurs en bash
+nano addUsers.sh
 
-Création du fichier script  
-![nanoaddusers](https://github.com/user-attachments/assets/d0648975-f3ef-4cf9-a77d-47be3a82af36)
+#!/bin/bash
 
-Contenu du fichier script  
-![scripttest](https://github.com/user-attachments/assets/5b6cdb97-35a5-4132-ae0a-95f2b488086a)  
+# Vérification de la présence d'arguments
+if [ "$#" -eq 0 ]; then
+  echo "Il manque les noms d'utilisateurs en argument - Fin du script"
+    exit 1
+    fi
+
+# Vérification de l'existence dans le système du/des nom(s) indiqué(s) en arguments  
+for user in "$@"; do  
+  if cat /etc/passwd | grep -q "$user"  
+  then
+    echo "L'utilisateur $user existe déjà"
+  else
+    # Création de l'utilisateur
+    useradd $user
+
+    # Vérification de la création de l'utilisateur
+    if [ $? -eq 0 ]; then
+      echo "L'utilisateur $user a été crée"
+    else
+      echo "Erreur à la création de l'utilisateur $user"
+    fi
+  fi
+done
